363c622bbe35224ba6bfa30f9286f1948a0f5023
Angel <angelplaiesu@gmail.com>
2023-10-20 00:07:00 +0200

    Responsive Desing 80% Done

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+let Options = {
+    "Frakt":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+    "Spedisjon":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+    "Rengskap":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+    "Fortoling":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+    "Rådgivning":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+    "Lagring":"Lorem ipsum dolor sit amet. consectetur. adipiseing elit ",
+}
+
+let subOptions = {
+    "Frakt":[],
+    "Spedisjon":[],
+    "Rengskap":[],
+    "Fortoling":[],
+    "Rådgivning":[],
+    "lagring":[],
+}
+
+var menu = document.getElementById('yourElementId');
+
+menu.addEventListener("mouseover", () => {
+
+});
